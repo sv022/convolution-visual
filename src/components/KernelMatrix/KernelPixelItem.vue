040df763d356591ea9dd0bb85004b4061aeb1996
sv022 <ssvatkin04@gmail.com>
2025-12-06 15:05:25 +0300

    feat: modify pixel hovercard

--- src/components/KernelMatrix/KernelPixelItem.vue
+++ src/components/KernelMatrix/KernelPixelItem.vue
@@ -1,117 +1,100 @@
-<script setup lang="ts">
-import grayscaleToHex from '@/utils/grayscaleToHex'
-import HoverCard from '../ui/hover-card/HoverCard.vue'
-import HoverCardContent from '../ui/hover-card/HoverCardContent.vue'
-import HoverCardTrigger from '../ui/hover-card/HoverCardTrigger.vue'
-import invertGrayscaleToHex from '@/utils/invertGrayscale'
-import { computed, ref, watch } from 'vue'
-import { useconv2dStore } from '@/stores/conv2d'
-import Label from '../ui/label/Label.vue'
-import Slider from '../ui/slider/Slider.vue'
-import { cn } from '@/lib/utils'
-import Button from '../ui/button/Button.vue'
+<script setup
+  lang="ts">
+  import grayscaleToHex from '@/utils/grayscaleToHex'
+  import HoverCard from '../ui/hover-card/HoverCard.vue'
+  import HoverCardContent from '../ui/hover-card/HoverCardContent.vue'
+  import HoverCardTrigger from '../ui/hover-card/HoverCardTrigger.vue'
+  import invertGrayscaleToHex from '@/utils/invertGrayscale'
+  import { computed, ref, watch } from 'vue'
+  import { useconv2dStore } from '@/stores/conv2d'
+  import Label from '../ui/label/Label.vue'
+  import Slider from '../ui/slider/Slider.vue'
+  import { cn } from '@/lib/utils'
+  import Button from '../ui/button/Button.vue'
 
-const conv2dStore = useconv2dStore()
+  const conv2dStore = useconv2dStore()
 
-const props = defineProps<{
-  value: number
-  pixelValue: number
-  posX: number
-  posY: number
-  size: string
-}>()
+  const props = defineProps<{
+    value: number
+    pixelValue: number
+    posX: number
+    posY: number
+    size: string
+  }>()
 
-const val = ref<number[]>([Math.round(props.value * 100) / 100])
-const pixelVal = computed<number>(() => props.pixelValue || 0)
+  const val = ref<number[]>([Math.round(props.value * 100) / 100])
+  const pixelVal = computed<number>(() => props.pixelValue || 0)
 
-const step = 0.01
+  const step = 0.01
 
-const pixelValue = computed({
-  get() {
-    return [Math.round(props.value * 100) / 100]
-  },
-  set(newValue) {
-    val.value = newValue
-  },
-})
+  const pixelValue = computed({
+    get() {
+      return [Math.round(props.value * 100) / 100]
+    },
+    set(newValue) {
+      val.value = newValue
+    },
+  })
 
-const pixelFontSize = computed<string>(() => {
-  if (props.size == 'size-7') return 'text-[12px]'
-  return 'text-md'
-})
+  const pixelFontSize = computed<string>(() => {
+    if (props.size == 'size-7') return 'text-[12px]'
+    return 'text-md'
+  })
 
-const kernelValueTextStyle = computed<string>(() => {
-  if (props.value > 0) return 'text-emerald-700'
-  else if (props.value < 0) return 'text-rose-700'
-  return 'text-neutral-700'
-})
+  const kernelValueTextStyle = computed<string>(() => {
+    if (props.value > 0) return 'text-emerald-700'
+    else if (props.value < 0) return 'text-rose-700'
+    return 'text-neutral-700'
+  })
 
-const bgColor = computed<string>(() => {
-  return grayscaleToHex(pixelVal.value)
-})
+  const bgColor = computed<string>(() => {
+    return grayscaleToHex(pixelVal.value)
+  })
 
-const setKernelPixel = () => {
-  conv2dStore.setKernelPixel(props.posX - 1, props.posY - 1, val.value[0])
-  return
-}
-const incrementKernelPixel = (step: number) => {
-  val.value[0] += step
-  setKernelPixel()
-}
+  const setKernelPixel = () => {
+    conv2dStore.setKernelPixel(props.posX - 1, props.posY - 1, val.value[0])
+    return
+  }
+  const incrementKernelPixel = (step: number) => {
+    val.value[0] += step
+    setKernelPixel()
+  }
 
-const decrementKernelPixel = (step: number) => {
-  val.value[0] -= step
-  setKernelPixel()
-}
+  const decrementKernelPixel = (step: number) => {
+    val.value[0] -= step
+    setKernelPixel()
+  }
 
-watch(
-  () => props.value,
-  () => (val.value[0] = props.value),
-)
+  watch(
+    () => props.value,
+    () => (val.value[0] = props.value),
+  )
 </script>
 
 <template>
   <HoverCard>
     <div class="flex flex-col items-center">
+
+      <div :class="cn('flex justify-center items-center border text-gray-500', props.size, pixelFontSize)
+        " :style="{ backgroundColor: bgColor, color: invertGrayscaleToHex(pixelVal) }">
+        {{ pixelVal.toFixed(2) }}
+      </div>
       <HoverCardTrigger>
-        <div
-          :class="
-            cn('flex justify-center items-center border text-gray-500', props.size, pixelFontSize)
-          "
-          :style="{ backgroundColor: bgColor, color: invertGrayscaleToHex(pixelVal) }"
-        >
-          {{ pixelVal.toFixed(2) }}
+        <div class="flex items-center justify-center h-10 w-10 text-nowrap text-md">
+          <mark>x <wbr /></mark><mark :class="kernelValueTextStyle">{{ props.value.toFixed(2) }}</mark>
         </div>
       </HoverCardTrigger>
-      <div class="flex items-center justify-center h-10 w-10 text-nowrap text-md">
-        <mark>x <wbr /></mark
-        ><mark :class="kernelValueTextStyle">{{ props.value.toFixed(2) }}</mark>
-      </div>
     </div>
     <HoverCardContent>
       <div class="space-y-2">
         <Label class="p-2" for="slider">Kernel value: {{ pixelValue }}</Label>
         <span class="flex">
-          <Button
-            variant="ghost"
-            @click="decrementKernelPixel(step)"
-            @click.shift.stop="decrementKernelPixel(step * 10)"
-            >-</Button
-          >
-          <Slider
-            id="slider"
-            v-model="pixelValue"
-            :min="-1"
-            :max="1"
-            :step="step"
-            @update:model-value="setKernelPixel"
-          />
-          <Button
-            variant="ghost"
-            @click="incrementKernelPixel(step)"
-            @click.shift.stop="incrementKernelPixel(step * 10)"
-            >+</Button
-          >
+          <Button variant="ghost" @click="decrementKernelPixel(step)"
+            @click.shift.stop="decrementKernelPixel(step * 10)">-</Button>
+          <Slider id="slider" v-model="pixelValue" :min="-1" :max="1" :step="step"
+            @update:model-value="setKernelPixel" />
+          <Button variant="ghost" @click="incrementKernelPixel(step)"
+            @click.shift.stop="incrementKernelPixel(step * 10)">+</Button>
         </span>
         <Label class="p-2">Position: ({{ props.posX }}, {{ props.posY }})</Label>
       </div>
--- src/components/PixelsMatrix/PixelItem.vue
+++ src/components/PixelsMatrix/PixelItem.vue
@@ -1,80 +1,93 @@
 <script setup
-    lang="ts">
-    import { cn } from '@/lib/utils';
-    import HoverCard from '../ui/hover-card/HoverCard.vue';
-    import HoverCardContent from '../ui/hover-card/HoverCardContent.vue';
-    import HoverCardTrigger from '../ui/hover-card/HoverCardTrigger.vue';
-    import grayscaleToHex from '@/utils/grayscaleToHex';
-    import { Slider } from '@/components/ui/slider'
-    import { Label } from '@/components/ui/label'
-    import { computed, ref } from 'vue';
-    import { useconv2dStore } from '@/stores/conv2d';
+  lang="ts">
+  import { cn } from '@/lib/utils';
+  import HoverCard from '../ui/hover-card/HoverCard.vue';
+  import HoverCardContent from '../ui/hover-card/HoverCardContent.vue';
+  import HoverCardTrigger from '../ui/hover-card/HoverCardTrigger.vue';
+  import grayscaleToHex from '@/utils/grayscaleToHex';
+  import { Slider } from '@/components/ui/slider'
+  import { Label } from '@/components/ui/label'
+  import { computed, ref } from 'vue';
+  import { useconv2dStore } from '@/stores/conv2d';
+  import { Button } from '../ui/button';
 
-    const conv2dStore = useconv2dStore()
+  const conv2dStore = useconv2dStore()
 
-    const props = defineProps<{
-        value: number
-        size: string
-        highlight: boolean
-        posX: number
-        posY: number
-    }>();
+  const props = defineProps<{
+    value: number
+    size: string
+    highlight: boolean
+    posX: number
+    posY: number
+  }>();
 
-    const pixelValue = ref<number[]>([Math.round(props.value * 100) / 100])
+  const step = 0.01
 
-    const width = computed(() => {
-        return conv2dStore.input.width + conv2dStore.padding * 2
-    })
-    const height = computed(() => {
-        return conv2dStore.input.height + conv2dStore.padding * 2
-    })
+  const pixelValue = ref<number[]>([Math.round(props.value * 100) / 100])
 
-    const updatePixel = (posX: number, posY: number, value: number) => {
-        conv2dStore.setImagePixel(posX - 1 - conv2dStore.padding, posY - 1 - conv2dStore.padding, value)
-        const r = Math.floor(conv2dStore.kernel.width / 2)
+  const width = computed(() => {
+    return conv2dStore.input.width + conv2dStore.padding * 2
+  })
+  const height = computed(() => {
+    return conv2dStore.input.height + conv2dStore.padding * 2
+  })
 
-        let w = posX;
-        let h = posY;
+  const updatePixel = (posX: number, posY: number, value: number) => {
+    if (value > 1) value = 1
+    if (value < 0) value = 0
+    pixelValue.value[0] = Math.round(value * 100) / 100
+    conv2dStore.setImagePixel(posX - 1 - conv2dStore.padding, posY - 1 - conv2dStore.padding, value)
+    const r = Math.floor(conv2dStore.kernel.width / 2)
 
-        if (w <= r) {
-            w = Math.round(r / 2);
-        } else if (w >= width.value - r) {
-            w = width.value - (r * 2)
-        } else {
-            w--
-        }
+    let w = posX;
+    let h = posY;
 
-        if (h <= r) {
-            h = Math.round(r / 2);
-        } else if (h >= height.value - r) {
-            h = height.value - (r * 2)
-        } else {
-            h--
-        }
-        // visualsStore.getHighlightFrame(w, h, r, conv2dStore.input)
+    if (w <= r) {
+      w = Math.round(r / 2);
+    } else if (w >= width.value - r) {
+      w = width.value - (r * 2)
+    } else {
+      w--
     }
+
+    if (h <= r) {
+      h = Math.round(r / 2);
+    } else if (h >= height.value - r) {
+      h = height.value - (r * 2)
+    } else {
+      h--
+    }
+    // visualsStore.getHighlightFrame(w, h, r, conv2dStore.input)
+  }
 
 
 </script>
 
 <template>
-    <div>
-        <HoverCard :open-delay="1500" :close-delay="1000">
-            <HoverCardTrigger>
-                <div :class="cn('flex items-center justify-center size-5 text-[8px] border text-gray-400', props.size, { 'border-red-500': props.highlight })"
-                    :style="{ backgroundColor: grayscaleToHex(props.value) }">
-                    {{ props.value.toFixed(2) }}
-                </div>
-            </HoverCardTrigger>
-            <HoverCardContent>
-                <div class="space-y-2">
-                    <Label class="p-2" for="slider">Pixel value: {{ pixelValue }}</Label>
-                    <Slider id="slider" v-model="pixelValue" :min="0" :max="1" :step="0.1"
-                        @update:model-value="updatePixel(props.posX, props.posY, pixelValue[0])" />
-                    <Label class="p-2">Position: ({{ props.posX }}, {{ props.posY }})</Label>
-                </div>
-            </HoverCardContent>
-        </HoverCard>
-    </div>
+  <div>
+    <HoverCard :open-delay="1500" :close-delay="1000">
+      <HoverCardTrigger>
+        <div
+          :class="cn('flex items-center justify-center size-5 text-[8px] border text-gray-400', props.size, { 'border-red-500': props.highlight })"
+          :style="{ backgroundColor: grayscaleToHex(props.value) }">
+          {{ props.value.toFixed(2) }}
+        </div>
+      </HoverCardTrigger>
+      <HoverCardContent>
+        <div class="space-y-2">
+          <Label class="p-2" for="slider">Pixel value: {{ pixelValue }}</Label>
+          <span class="flex">
+            <Button variant="ghost" @click="updatePixel(props.posX, props.posY, pixelValue[0] - step)"
+              @click.shift.stop="updatePixel(props.posX, props.posY, pixelValue[0] - step * 10)">-</Button>
+            <Slider id="slider" v-model="pixelValue" :min="0" :max="1" :step="0.05"
+              @update:model-value="updatePixel(props.posX, props.posY, pixelValue[0])" />
+            <Button variant="ghost" @click="updatePixel(props.posX, props.posY, pixelValue[0] + step)"
+              @click.shift.stop="updatePixel(props.posX, props.posY, pixelValue[0] + step * 10)">-</Button>
+          </span>
+          <Label class="p-2">Position: ({{ props.posX }}, {{ props.posY }})</Label>
+        </div>
+      </HoverCardContent>
+    </HoverCard>
+  </div>
 
 </template>
