bf75eb37dff56a2f4ccaeb18ef464dbda47f5409
sv022 <ssvatkin04@gmail.com>
2026-03-15 17:18:06 +0300

    feat: update homepage layout and images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,20 +4,18 @@
 </script>
 
 <template>
-  <main>
-    <div class="flex flex-col items-center justify-center p-10 space-y-10">
-      <RouterLink to="/conv2d">
-        <div class="border p-2 border-gray-500 rounded-md">
-          <img src="../assets/homepage_demo2.png" alt="demo" width="800" />
-          <h2 class="text-2xl font-bold text-center">Convolution Grayscale</h2>
-        </div>
-      </RouterLink>
-      <RouterLink to="/convrgb">
-        <div class="border p-2 border-gray-500 rounded-md">
-          <img src="../assets/homepage_demo3.png" alt="demo" width="800" />
-          <h2 class="text-2xl font-bold text-center">Convolution RGB</h2>
-        </div>
-      </RouterLink>
-    </div>
+  <main class="md:flex justify-center space-x-10 items-center md:py-[20dvh]">
+    <RouterLink to="/conv2d">
+      <div class="border p-2 border-gray-500 rounded-md">
+        <img src="../assets/homepage_demo2.png" alt="demo" width="600" />
+        <h2 class="text-2xl font-bold text-center">Convolution Grayscale</h2>
+      </div>
+    </RouterLink>
+    <RouterLink to="/convrgb">
+      <div class="border p-2 border-gray-500 rounded-md">
+        <img src="../assets/homepage_demo3.png" alt="demo" width="600" />
+        <h2 class="text-2xl font-bold text-center">Convolution RGB</h2>
+      </div>
+    </RouterLink>
   </main>
 </template>
